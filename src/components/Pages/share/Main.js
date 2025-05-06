4ae8ab251f7fbdd6ca1ebd18463d0a1d14047781
bttrainees123 <bttrainees1@gmail.com>
2025-05-06 14:36:11 +0530

    ...

--- src/components/Pages/share/Main.js
+++ src/components/Pages/share/Main.js
@@ -1,46 +1,59 @@
 import React, { useRef, useState } from 'react';
 import html2canvas from 'html2canvas';
 import { FacebookIcon, FacebookShareButton } from 'react-share';
-import img from '../../image/d1.jpg'
+import img from '../../image/d1.jpg';
+import axios from 'axios';
 
 const Main = () => {
     const targetElement = useRef(null);
     const [screenshotUrl, setScreenshotUrl] = useState('');
 
     const handleScreenshot = async () => {
-        const canvas = await html2canvas(targetElement.current);
-        const screenshotData = canvas.toDataURL('image/png');
-        setScreenshotUrl(screenshotData);
+        try {
+            const canvas = await html2canvas(targetElement.current);
+            const screenshotData = canvas.toDataURL('image/png');
+            console.log("screenshotData", screenshotData);
+            
+
+            const response = await axios.post(`https://n-again.com/api/uploadbase64`, { image: screenshotData });
+
+            if (response.data?.status) {
+                const uploadedUrl = 'https://n-again.com/images/' + response.data.data;
+                setScreenshotUrl(uploadedUrl);
+
+                const fbShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(uploadedUrl)}`;
+                window.open(fbShareUrl, '_blank');
+            } else {
+                console.error("Something went wrong during image upload");
+            }
+        } catch (error) {
+            console.error("Error capturing or uploading screenshot:", error);
+        }
     };
+
     return (
         <div>
-            
             <div ref={targetElement}>
                 <img src={img} alt='' />
                 <h1>This is the content to be screenshot</h1>
                 <p>Some more content...</p>
             </div>
 
-            <button onClick={handleScreenshot}>Take Screenshot</button>
+            <button onClick={handleScreenshot}>
+                <FacebookIcon size={40} round />
+            </button>
 
-            <div style={{ border: '2px solid black' }}>
-                {screenshotUrl && (
-                    <img src={screenshotUrl} alt='' />
-                    
-                )}
+            <div style={{ border: '2px solid black', marginTop: '10px' }}>
+                {screenshotUrl && <img src={screenshotUrl} alt='Screenshot' />}
             </div>
 
-
-            {screenshotUrl && (
-                <FacebookShareButton
-                    url={screenshotUrl}
-                    className="SocialMediaShareButton"
-                >
-                    <FacebookIcon size={32} round />
+            {/* {screenshotUrl && (
+                <FacebookShareButton url={screenshotUrl} className="SocialMediaShareButton">
+                    <FacebookIcon size={40} round />
                 </FacebookShareButton>
-            )}
+            )} */}
         </div>
-    )
-}
+    );
+};
 
-export default Main
+export default Main;
